1e44ec351048313801e99d87f0717bf0f66ee703
dhruv-m-patel <dhruvpatel.work@gmail.com>
2019-09-01 18:21:10 -0600

    Cleanup file after download

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import ServiceFactory from '../services/service-factory';
 import { getPageHtml } from '../lib/utils';
 
@@ -14,7 +15,12 @@ router.get('/', async (req, res) => {
 router.post('/pdf', async (req, res) => {
   const imdbDataService = ServiceFactory.getImdbDataService(req);
   const report = await imdbDataService.generateReport();
-  res.download(report);
+  imdbDataService.logger.info('Sending file...');
+  res.setHeader("content-type", "application/pdf");
+  fs.createReadStream(report).pipe(res);
+  imdbDataService.logger.info('File sent, removing file from temp directory now');
+  fs.unlinkSync(report);
+  imdbDataService.logger.info('File removed', report);
 });
 
 export default router;
